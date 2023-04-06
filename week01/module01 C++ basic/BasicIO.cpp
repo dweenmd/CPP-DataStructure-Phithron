95a0a38835bfe1eb728056a9a6b411059e980c83
Dween MOhammad <dween.md01@gmail.com>
2023-04-06 22:06:20 +0600

    update

--- week01/module01 C++ basic/BasicIO.cpp
+++ week01/module01 C++ basic/BasicIO.cpp
@@ -5,13 +5,16 @@ int main()
     // variable declare
     int a,b;
     // input
+    cout<<"input 2 integer:";
     cin >> a >>b ;   //>> excertion operator
     //output
+    cout<<"output of 2 integer:";
     cout <<a << " " << b<<"\n"; //<<is called incurtion operator
 
     char ch;
     float f;
     double d;
+    cout<<"input character, float , integer :";
     cin>> ch>>f>> d;
     cout << ch <<" "<<f<< " "<<d <<"\n";
     return 0;
