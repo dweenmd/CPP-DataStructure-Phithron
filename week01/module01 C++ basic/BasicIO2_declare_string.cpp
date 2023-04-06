08409bf377cd7a0f91a7a17c7de004e277c9f41f
Dween MOhammad <dween.md01@gmail.com>
2023-04-06 22:15:06 +0600

    up

--- week01/module01 C++ basic/BasicIO2_declare_string.cpp
+++ week01/module01 C++ basic/BasicIO2_declare_string.cpp
@@ -1,18 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-    //string declare
+    // string declare
     string a;
-    //full string input with space
-    getline(cin , a);
-    cout << a<<"\n";
 
-    string s= "My name is Dween Mohammad";
-    string s2= " And What is your name?";
-    s= s+s2;
-    cout <<s << "\n";
+    // full string input with space
+    cout << "input a string: ";
+    getline(cin, a);
+    
+    cout << "Output of string:\n";
+    cout << a << "\n";
+    // adding 2 string{concate string}
 
+    cout << "String Concatenation:\n";
+
+    string s = "My name is Dween Mohammad";
+    string s2 = " And What is your name?";
+    s = s + s2;
+    cout << s << "\n";
 
     return 0;
 }
