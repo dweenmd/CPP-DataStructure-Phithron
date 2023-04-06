15086ffdb8be736787221b09828f092c6764f25e
Dween MOhammad <dween.md01@gmail.com>
2023-04-06 22:50:21 +0600

    m

--- week01/module01 C++ basic/BasicIO2_file_input_output.cpp
+++ week01/module01 C++ basic/BasicIO2_file_input_output.cpp
@@ -1,14 +1,25 @@
-#include<iostream>
+#include <iostream>
 
 using namespace std;
 
+/*
 int main()
 {
-    freopen("input.txt","r",stdin);//file theke input nicche 
+    freopen("input.txt","r",stdin);//file theke input nicche
     string s;
     //cin>>s;
     getline(cin , s);
     cout<<s<<"\n";
 return 0;
 }
+*/
 
+#include <fstream>
+int main()
+{
+    ofstream of; // file output er jonno ofstream ar input er jonno  ifstream;
+    of.open("output.txt");
+    // cout er jaygay of lehelie outpur asbe
+
+    of << "hello world\n" //output.txt te giye print hobe
+}
