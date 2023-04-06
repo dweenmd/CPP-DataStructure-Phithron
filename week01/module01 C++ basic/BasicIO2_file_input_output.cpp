550f8e92997c98e1bca57fb2ce91510550c303d1
Dween MOhammad <dween.md01@gmail.com>
2023-04-06 22:35:39 +0600

    m

--- week01/module01 C++ basic/BasicIO2_file_input_output.cpp
+++ week01/module01 C++ basic/BasicIO2_file_input_output.cpp
@@ -1,1 +1,14 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    freopen("input.txt","r",stdin);//file theke input nicche 
+    string s;
+    //cin>>s;
+    getline(cin , s);
+    cout<<s<<"\n";
+return 0;
+}
 
