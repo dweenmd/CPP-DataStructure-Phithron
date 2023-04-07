cd1e666653eff07bc399aaf06c1bdc8f361b9bf9
Dween MOhammad <dween.md01@gmail.com>
2023-04-08 05:45:08 +0600

    update

--- week01/module01 C++ basic/BasicIO2_file_input_output.cpp
+++ week01/module01 C++ basic/BasicIO2_file_input_output.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
@@ -14,12 +15,24 @@ return 0;
 }
 */
 
-#include <fstream>
+
 int main()
 {
     ofstream of; // file output er jonno ofstream ar input er jonno  ifstream;
     of.open("output.txt");
     // cout er jaygay of lehelie outpur asbe
 
-    of << "hello world\n" //output.txt te giye print hobe
+    of << "hello world\n" ;//output.txt te giye print hobe
+
+    ifstream ifs;
+    ifs.open("open.txt");
+    int x;
+    double y,z;
+    ifs >>  x >> y >> z;
+    cout <<x<< " "<<y<<" "<<z<<"\n";
+    ifs.close ();
+    of.close();
+
+
+    return 0;
 }
