e4819f68880f599d03a26a2dff27cbaf01d47d87
Dween MOhammad <dween.md01@gmail.com>
2023-04-11 13:09:20 +0600

    m

---
+++ week01/module02 Arrray,string,function,pointer/string.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s="i am deen ";
+    //print string s er lenth
+    cout<<s.size()<<"\n"; 
+    string s1="i am not deen";
+    string s2=s+s1;
+    cout<<s2<<endl;
+}
